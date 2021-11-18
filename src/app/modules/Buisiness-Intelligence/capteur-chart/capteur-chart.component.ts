f0c6e072424b7404a507161094451684546b3069
loica <loicarchimbaud@gmail.com>
2021-11-18 20:16:59 +0100

    Travail sur le chart

--- src/app/modules/Buisiness-Intelligence/capteur-chart/capteur-chart.component.ts
+++ src/app/modules/Buisiness-Intelligence/capteur-chart/capteur-chart.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Chart } from "node_modules/chart.js";
 
 @Component({
-  selector: 'app-capteur-chart',
-  templateUrl: './capteur-chart.component.html',
-  styleUrls: ['./capteur-chart.component.scss']
+  selector: "app-capteur-chart",
+  templateUrl: "./capteur-chart.component.html",
+  styleUrls: ["./capteur-chart.component.scss"],
 })
 export class CapteurChartComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    let ctx = document.getElementById("capteurChart");
+    let myChart = new Chart(ctx, {});
   }
-
 }
