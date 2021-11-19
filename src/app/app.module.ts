7ece70bcd3128b7154ab77c1a0413f178c80bf6f
loica <loicarchimbaud@gmail.com>
2021-11-19 23:51:26 +0100

    Ajout de chart.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { ToastrModule } from "ngx-toastr";
+import { ChartModule } from "primeng/chart";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { LoginComponent } from "./modules/login/login.component";
-import { HomeComponent } from "./modules/Back-office/home/home.component";
-import { HeaderComponent } from "./modules/header/header.component";
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
-import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { RoleGuard } from "./guard/role.guard";
 import { IsSignedInGuard } from "./guard/isSignedGuard";
+import { RoleGuard } from "./guard/role.guard";
 import { AuthInterceptor } from "./helpers/auth.interceptor";
+import { HomeComponent } from "./modules/Back-office/home/home.component";
 import { ProfileComponent } from "./modules/Back-office/profile/profile.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { ToastrModule } from "ngx-toastr";
+import { CapteurChartComponent } from "./modules/Buisiness-Intelligence/capteur-chart/capteur-chart.component";
 import { CapteurValueComponent } from "./modules/Buisiness-Intelligence/capteur-value/capteur-value.component";
-import { CapteurChartComponent } from './modules/Buisiness-Intelligence/capteur-chart/capteur-chart.component';
-
+import { HeaderComponent } from "./modules/header/header.component";
+import { LoginComponent } from "./modules/login/login.component";
 @NgModule({
   declarations: [
     AppComponent,
@@ -36,6 +36,7 @@ import { CapteurChartComponent } from './modules/Buisiness-Intelligence/capteur-
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
+    ChartModule,
     ToastrModule.forRoot(),
   ],
   providers: [
